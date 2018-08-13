cb8b8f8153dd4cc1131d0c1c107b78090c56d3e4
LaborExperiment <42354920+LaborExperiment@users.noreply.github.com>
2018-08-13 20:16:30 +0200

    hab join und quit message hinzugefügt UND wenn man am anfang vom chat $ eingibt wird die nachricht bunt
    übrigens bin Putzeimer vom Server, aber kannst du mich bitte wieder entbannen ich wusste nicht das man für 5 mal hintereinander ne Nachricht schreiben sofort gebannt wird habe mal meinen freund Jerrycrafter05 gerufen tut mir auch leid das ich das so oft hintereinander geschrieben hab kannst du mich bitte wieder entbannen?

--- src/main/java/comDot0815Craft/EventHandlerClass.java
+++ src/main/java/comDot0815Craft/EventHandlerClass.java
@@ -3,6 +3,9 @@ package comDot0815Craft;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import java.lang.StringBuilder;
 
 public class EventHandlerClass implements Listener {
 
@@ -14,5 +17,38 @@ public class EventHandlerClass implements Listener {
             return;
         }
         chatEvent.setFormat("%s ➮ %s");
+        
+        //rainbow color wenn nachricht mit $ anfängt
+        if(chatEvent.getMessage().startsWith("$")) {
+            chatEvent.setMessage(color(chatEvent.getMessage().substring(1)));
+        }
+    }
+    
+   private String color(String s) {
+       char[] colors = new char[]{'4', '6', 'e', 'a', '2', 'b', '9', '5', 'd'};
+        int i = -1;
+        StringBuilder builder = new StringBuilder();
+        for(char c : s.toCharArray()) {
+            if(c == ' ') {
+                builder.append(' ');
+                continue;
+            }
+            i++;
+            builder.append("§").append(colors[i]).append(c);
+            if(i == 8) {
+                i = -1;
+            }
+        }
+        return builder.toString();
+   }
+    
+    @EventHandler
+    public void join(PlayerJoinEvent event) {
+        event.setJoinMessage("§a" + event.getPlayer().getName() + " §1hat §c0815Craft §1betreten§e!");
+    }
+    
+    @EventHandler
+    public void quit(PlayerQuitEvent event) {
+        event.setJoinMessage("§a" + event.getPlayer().getName() + " §1hat §c0815Craft §1verlassen§e!");
     }
 }
